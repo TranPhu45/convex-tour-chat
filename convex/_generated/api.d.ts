f97ac877650ae79f2b5a8657835f95205819e79f
PeterVo_45 <127285590+TranPhu45@users.noreply.github.com>
2024-09-24 14:43:03 +0700

    Add files via upload

--- convex/_generated/api.d.ts
+++ convex/_generated/api.d.ts
@@ -15,8 +15,10 @@ import type {
   FilterApi,
   FunctionReference,
 } from "convex/server";
+import type * as ai from "../ai.js";
 import type * as init from "../init.js";
 import type * as messages from "../messages.js";
+import type * as users from "../users.js";
 
 /**
  * A utility for referencing Convex functions in your app's API.
@@ -27,8 +29,10 @@ import type * as messages from "../messages.js";
  * ```
  */
 declare const fullApi: ApiFromModules<{
+  ai: typeof ai;
   init: typeof init;
   messages: typeof messages;
+  users: typeof users;
 }>;
 export declare const api: FilterApi<
   typeof fullApi,
