573bff28595728c3ca8620697c49dc0a1a4f5118
PeterVo_45 <127285590+TranPhu45@users.noreply.github.com>
2024-09-24 15:00:50 +0700

    Update badge.ts

--- src/badge.ts
+++ src/badge.ts
@@ -1,6 +1,1 @@
-import { useUser } from "@clerk/clerk-react";
 
-export default function Badge() {
-  const { user } = useUser();
-  return <span>Logged in as {user?.fullName ?? "Unknown User"}</span>;
-}
